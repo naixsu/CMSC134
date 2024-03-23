163c083619996095dd63b09e73ea59ef217bfaee
naixsu <plnacua02@gmail.com>
2024-03-23 13:37:00 +0800

    mp2_2.py final

---
+++ mp2/mp2_2.py
@@ -0,0 +1,86 @@
+from typing import Union
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+
+def generate_keypair(num_bytes: int) -> tuple[bytes, bytes]:
+    if num_bytes < 1024: # RSA modulus length must be >= 1024
+        num_bytes = 1024 
+    key = RSA.generate(num_bytes)
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    return private_key, public_key
+
+def encrypt_then_sign(message: Union[str, bytes], 
+                      pbk_encryption: bytes, 
+                      pvk_signing: bytes) -> tuple[bytes, bytes]:
+    
+    ## Encryption part
+    if isinstance(message, str): # Convert string to bytes if it's a string
+        message = message.encode()  
+    
+    key = RSA.import_key(pbk_encryption)
+    cipher = PKCS1_OAEP.new(key)
+    ciphertext = cipher.encrypt(message)
+    
+    ## Signature part
+    key = RSA.import_key(pvk_signing)
+    hash = SHA256.new(ciphertext)
+    signature = pkcs1_15.new(key).sign(hash)
+
+    return ciphertext, signature
+
+def verify_then_decrypt(encrypted_message: bytes,
+                        signature: bytes,
+                        pvk_encryption: bytes,
+                        pbk_signing: bytes) -> str:
+    
+    key = RSA.import_key(pbk_signing)
+    hash = SHA256.new(encrypted_message)
+    
+    try:
+        pkcs1_15.new(key).verify(hash, signature)
+        # Verified
+        key = RSA.import_key(pvk_encryption)
+        cipher = PKCS1_OAEP.new(key)
+        decrypted_message = cipher.decrypt(encrypted_message).decode()
+        return decrypted_message
+
+    except (ValueError, TypeError):
+        return "Verification Failed"
+    
+
+def main() -> None:
+    num_bytes = 1024 # Keep this at >=1024. Idk why it's at min 1024
+    private_key_encryption, public_key_encryption = generate_keypair(num_bytes=num_bytes)
+    private_key_signing, public_key_signing = generate_keypair(num_bytes=num_bytes)
+    
+    message = "Hello"
+    if len(message) > 140:
+        print("Message over the character limit.")
+        return 
+
+    print(f"Original Message: {message}\n")
+    
+    encrypted_message, signature = encrypt_then_sign(
+        message=message,
+        pbk_encryption=public_key_encryption,
+        pvk_signing=private_key_signing
+    )
+
+    print(f"Encrypted message: {encrypted_message}\n")
+    print(f"Signature: {signature}\n")
+    
+    decrypted_message = verify_then_decrypt(
+        encrypted_message=encrypted_message,
+        signature=signature,
+        pvk_encryption=private_key_encryption,
+        pbk_signing=public_key_signing
+    )
+    
+    print(f"Decrypted message: {decrypted_message}\n")
+
+if __name__ == "__main__":
+    main()
--- mp2/test.py
+++ mp2/test.py
@@ -1,15 +1,34 @@
-# This is for godot integration soon™️ experience
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
-import sys
+# simple_key = get_random_bytes(32)
+# print(simple_key)
 
-def test_function(test):
-    print(f"Testing function: {test}")
+salt = b'W\xeaIg\xf5\x90\x138\x19\xad8Q\x85\xd5\xa1\x8e\xfa\xea\xa2\xb2\xfa\x8fsflmVv{\xe2)\xb9'
+password = "mypassword"
 
-def main():
-    args = sys.argv
-    test_function(args[1])
+key = PBKDF2(password, salt, dkLen=32)
+
+message = b"Hello"
+
+cipher = AES.new(key, AES.MODE_CBC)
+ciphered_data = cipher.encrypt(pad(message, AES.block_size))
+
+
+with open('encrypted.bin', 'wb') as f:
+    f.write(cipher.iv)
+    f.write(ciphered_data)
     
-
-if __name__ == "__main__":
-    main()
+with open('encrypted.bin', 'rb') as f:
+    iv = f.read(16)
+    decrypt_data = f.read()
     
+cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
+
+print(original)
+
+with open('key.bin', 'wb') as f:
+    f.write(key)
