748a447d7d5a7a1c341a1a9bfb23c44a822a1807
naixsu <plnacua02@gmail.com>
2024-05-10 21:40:03 +0800

    make session_token secure and httponly

--- mp3/app.py
+++ mp3/app.py
@@ -36,7 +36,7 @@ def login():
                         + str(user[0]) + ", '" + token + "');")
             con.commit()
             response = redirect("/home")
-            response.set_cookie("session_token", token)
+            response.set_cookie("session_token", token, secure=True, httponly=True)
             return response
         else:
             return render_template("login.html", error="Invalid username and/or password!")
